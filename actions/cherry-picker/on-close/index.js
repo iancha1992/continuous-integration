35697ca4c3e464173bf701bca1be0bac3a76fd2c
iancha1992 <heec@google.com>
2023-06-08 14:19:24 -0700

    created the getPRevents

--- actions/cherry-picker/on-close/index.js
+++ actions/cherry-picker/on-close/index.js
@@ -1,28 +1,22 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-
-
 const token = core.getInput("token");
 const octokit = github.getOctokit(token);
 
-
 console.log("This is the real. Testing adam");
 const payload = github.context.payload;
-
 console.log(payload);
+const issue_number = payload.number;
 
-
-async function run() {
-    const commentsResponse = await octokit.request(`GET /repos/iancha1992/gh_practice/issues/${issue_number}/comments`, {
+async function getPrEvents() {
+    const gitIssueEventsResponse = await octokit.request(`GET /repos/iancha1992/gh_practice/issues/${issue_number}/events`, {
         headers: {
             'X-GitHub-Api-Version': '2022-11-28'
         }
     });
+    return gitIssueEventsResponse.data
+};
 
+const prEvents = getPrEvents();
 
-    console.log("Comments?")
-    console.log(commentsResponse.data)
-}
-
-run();
-
+console.log(prEvents);
