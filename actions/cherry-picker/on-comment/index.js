dc551a82f66666e886e546cc6ca33a441ad80ba6
iancha1992 <heec@google.com>
2023-06-11 00:34:23 -0700

    testing

--- actions/cherry-picker/on-comment/index.js
+++ actions/cherry-picker/on-comment/index.js
@@ -13,9 +13,11 @@ const issue_number = payload.number;
 const pr_number = payload.issue.body.split("#")[1];
 console.log("This is my pr number", pr_number);
 
+actor_name = "copybara-service[bot]";
 
-async function getPrEventsInfos() {
-    const response = await octokit.request(`GET /repos/bazelbuild/bazel/issues/18130/comments`, {
+
+async function getIssueEventsInfos() {
+    const response = await octokit.request(`GET /repos/bazelbuild/bazel/issues/18130/events`, {
         headers: {
             'X-GitHub-Api-Version': '2022-11-28'
         }
@@ -23,23 +25,40 @@ async function getPrEventsInfos() {
     return response.data;
 };
 
+async function getPrEventsInfos() {
+    const response = await octokit.request(`GET /repos/bazelbuild/bazel/pulls/18130`, {
+        headers: {
+            'X-GitHub-Api-Version': '2022-11-28'
+        }
+    });
+    return response.data;
+}
+
+
+https://api.github.com/repos/bazelbuild/bazel/pulls/18130
+
 let commitId;
 
-Promise.all([getPrEventsInfos()])
+Promise.all([getPrEventsInfos(), getIssueEventsInfos()])
     .then((responses) => {
         console.log("Congress")
         console.log(responses);
 
-        for (const response of responses[0]) {
-            if ((response.actor.login == "copybara-service[bot]") && (response.commit_id != null) && (commitId == null)) {
+        if (responses[0].state != "closed") {
+            throw (`Pull Request ${pr_number} is not closed yet. Only closed ones are cherry-pickable.`)
+        }
+
+        for (const response of responses[1]) {
+            if ((response.actor.login == actor_name) && (response.commit_id != null) && (commitId == null)) {
                 commitId = response.commit_id
                 console.log("FAnta");
                 console.log(commitId)
             }
-            else if ((response.actor.login == "copybara-service[bot]") && (response.commit_id != null) && (commitId != null)) {
+            else if ((response.actor.login == actor_name) && (response.commit_id != null) && (commitId != null)) {
                 console.log("Anotherone")
                 console.log(commitId)
                 throw "There are multiple commits made by copybara-service[bot]. There can only be one."
             }
         }
+
     })
