2749e71618083644ff4db0641a5ec5195ad3ebb6
iancha1992 <heec@google.com>
2023-06-12 15:58:51 -0700

    Testing

--- actions/cherry-picker/cherry-pick-runner/index.js
+++ actions/cherry-picker/cherry-pick-runner/index.js
@@ -1,3 +1,5 @@
 function cherrypickRunner(commitId, prNumber, token, reviewer) {
-    console.log(`Running cherrypicking with commitID ${commitId}, PR#${prNumber}, Reviewer ${reviewer}`);
+    console.log(`Running cherrypicking with commitID ${commitId}, PR#${prNumber}`);
+    console.log("Reviewers!!!");
+    console.log(reviewer);
 }
