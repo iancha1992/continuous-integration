0da6513ac7715828cf5445dc45f4091e065c0d21
iancha1992 <heec@google.com>
2023-06-12 16:21:00 -0700

    testing

--- actions/cherry-picker/cherry-pick-runner/index.js
+++ actions/cherry-picker/cherry-pick-runner/index.js
@@ -1,4 +1,4 @@
-export function cherrypickRunner(commitId, prNumber, token, reviewer) {
+const cherrypickRunner = (commitId, prNumber, token, reviewer) => {
     console.log(`Running cherrypicking with commitID ${commitId}, PR#${prNumber}`);
     console.log("Reviewers!!!");
     console.log(reviewer);
