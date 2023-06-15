b6315da98d7ba49e276f847514e069dc7b86ace8
iancha1992 <heec@google.com>
2023-06-15 15:36:56 -0700

    Testing

--- actions/cherry-picker/cherry-pick-runner/index.js
+++ actions/cherry-picker/cherry-pick-runner/index.js
@@ -2,7 +2,8 @@ const cherrypickRunner = (commitId, prNumber, token, reviewer, releaseNumber) =>
     console.log(`Running cherrypicking with commitID ${commitId}, PR#${prNumber}`);
     console.log("Reviewers!!!");
     console.log(reviewer);
-    console.log("This is the releaseNumber", releaseNumber);
+    console.log("This is the releaseNumber");
+    console.log(releaseNumber);
 }
 
 module.exports = cherrypickRunner;
