41e6efa9333ef11c60a84a971706d84c0f31e9f6
iancha1992 <heec@google.com>
2023-06-21 14:29:22 -0700

    Testing

--- actions/cherry-picker/functions/check_closed.py
+++ actions/cherry-picker/functions/check_closed.py
@@ -4,8 +4,8 @@ def check_closed(pr_number):
     headers = {
         'X-GitHub-Api-Version': '2022-11-28'
     }
-    r = requests.get(f'https://api.github.com/repos/iancha1992/bazel/pulls/{pr_number}', headers = headers)
-    print("hihihihi", r.text.state)
+    r = requests.get(f'https://api.github.com/repos/iancha1992/bazel/pulls/{pr_number}', headers=headers)
+    print("hihihihi", r.json())
     return True if r.text.state == "closed" else False
     
 
