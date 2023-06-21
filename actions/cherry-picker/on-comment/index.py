55761dbb86094e12487f6139036fce5b4f2042b2
iancha1992 <heec@google.com>
2023-06-21 13:25:12 -0700

    Testing

--- actions/cherry-picker/on-comment/index.py
+++ actions/cherry-picker/on-comment/index.py
@@ -1,7 +1,8 @@
 import os
 token = os.environ["INPUT_TOKEN"]
-pr_number = os.environ["INPUT_PR_NUMBER"]
 triggered_on = os.environ["INPUT_TRIGGERED_ON"]
+pr_number = os.environ["INPUT_PR_NUMBER"] if triggered_on == "closed" else os.environ["INPUT_PR_NUMBER"].split("#")[1]
+
 
 print("This is the payload!")
 print(pr_number)
