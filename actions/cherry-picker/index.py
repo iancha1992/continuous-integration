31f7f85a4102dbe5a027c1eb1565af3f747ee713
iancha1992 <heec@google.com>
2023-06-21 13:35:59 -0700

    Testing

--- actions/cherry-picker/index.py
+++ actions/cherry-picker/index.py
@@ -4,3 +4,9 @@ triggered_on = os.environ["INPUT_TRIGGERED_ON"]
 pr_number = os.environ["INPUT_PR_NUMBER"] if triggered_on == "closed" else os.environ["INPUT_PR_NUMBER"].split("#")[1]
 
 print("This is prnumber!!!!!!!", pr_number)
+
+
+
+
+
+
