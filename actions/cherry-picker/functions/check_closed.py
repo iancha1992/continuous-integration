8e1712aa1e16c2b7a766caec4d7af4d4c4aacaa6
iancha1992 <heec@google.com>
2023-06-21 14:04:32 -0700

    Testing

--- actions/cherry-picker/functions/check_closed.py
+++ actions/cherry-picker/functions/check_closed.py
@@ -1,7 +1,7 @@
 import requests
 def check_closed(pr_number):
     print("Closing!")
-    r = requests.get(f'/repos/iancha1992/bazel/pulls/{pr_number}')
+    r = requests.get(f'https://api.github.com/repos/iancha1992/bazel/pulls/{pr_number}')
     print(r.text)
     
 
