4e91c136ebbf104204524fa771e097774df1d31f
iancha1992 <heec@google.com>
2023-06-21 16:40:23 -0700

    Testing

--- actions/cherry-picker/functions/extract_release_numbers_data.py
+++ actions/cherry-picker/functions/extract_release_numbers_data.py
@@ -7,11 +7,14 @@ def get_all_milestones_data():
     }
     r = requests.get(f'https://api.github.com/repos/iancha1992/bazel/milestones', headers=headers)
     print("tupac")
-    pprint(r.json())
-    return r.json()
+    # pprint(r.json())
+
+    milestones_data = list(map(lambda n: {"title": n["title"].split("release blockers")[0].replace(" ", ""), "number": n["number"]}, r.json()))
+    return milestones_data
 
 
 def extract_release_numbers_data():
     milestones_data = get_all_milestones_data()
+    print("This is milestones_Datss", milestones_data)
 
     
