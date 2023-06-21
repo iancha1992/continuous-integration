93f7cff698b72f0b41ea1d9a680c8cad0fd861c0
iancha1992 <heec@google.com>
2023-06-21 13:20:54 -0700

    Testing

--- actions/cherry-picker/on-comment/index.py
+++ actions/cherry-picker/on-comment/index.py
@@ -1,6 +1,6 @@
 import os
 token = os.environ["INPUT_TOKEN"]
-pr_number = os.environ["PR_NUMBER"]
+pr_number = os.environ["INPUT_PR_NUMBER"]
 triggered_on = os.environ["INPUT_TRIGGERED_ON"]
 
 print("This is the payload!")
