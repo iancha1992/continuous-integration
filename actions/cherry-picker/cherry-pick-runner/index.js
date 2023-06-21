775cebe5b3753348d0da898aa32356af1249914a
iancha1992 <heec@google.com>
2023-06-21 13:30:15 -0700

    Testing

--- actions/cherry-picker/cherry-pick-runner/index.js
+++
@@ -1,9 +0,0 @@
-const cherrypickRunner = (commitId, prNumber, token, reviewer, releaseNumber) => {
-    console.log(`Running cherrypicking with commitID ${commitId}, PR#${prNumber}`);
-    console.log("Reviewers!!!");
-    console.log(reviewer);
-    console.log("This is the releaseNumber");
-    console.log(releaseNumber);
-}
-
-module.exports = cherrypickRunner;
--- actions/cherry-picker/index.py
+++ actions/cherry-picker/index.py
@@ -3,7 +3,3 @@ token = os.environ["INPUT_TOKEN"]
 triggered_on = os.environ["INPUT_TRIGGERED_ON"]
 pr_number = os.environ["INPUT_PR_NUMBER"] if triggered_on == "closed" else os.environ["INPUT_PR_NUMBER"].split("#")[1]
 
-
-print("This is the payload!")
-print(pr_number)
-
