00971c71d925b08702e7e586801dbeeaa66ecaa5
iancha1992 <heec@google.com>
2023-06-21 14:37:50 -0700

    Testing

--- actions/cherry-picker/functions/check_closed.py
+++ actions/cherry-picker/functions/check_closed.py
@@ -9,8 +9,5 @@ def check_closed(pr_number):
     r = requests.get(f'https://api.github.com/repos/iancha1992/bazel/pulls/{pr_number}', headers=headers)
     print("Dataha")
     pprint(r.json())
-    return True if r.json().state == "closed" else False
-    
-
-
-
+    if r.json().state == "closed": return True
+    return False
