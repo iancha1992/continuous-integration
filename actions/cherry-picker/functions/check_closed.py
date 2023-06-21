c62634ae800f769f102f8a0992a7de20a214fa47
iancha1992 <heec@google.com>
2023-06-21 13:59:01 -0700

    Testing

--- actions/cherry-picker/functions/check_closed.py
+++ actions/cherry-picker/functions/check_closed.py
@@ -1,3 +1,9 @@
+import requests
+def check_closed(pr_number):
+    print("Closing!")
+    r = requests.get(f'/repos/iancha1992/bazel/pulls/{pr_number}')
+    print(r.text)
+    
 
 
 
--- actions/cherry-picker/index.py
+++ actions/cherry-picker/index.py
@@ -1,4 +1,5 @@
 import os
+from functions.check_closed import check_closed
 
 
 token = os.environ["INPUT_TOKEN"]
@@ -6,9 +7,5 @@ triggered_on = os.environ["INPUT_TRIGGERED_ON"]
 pr_number = os.environ["INPUT_PR_NUMBER"] if triggered_on == "closed" else os.environ["INPUT_PR_NUMBER"].split("#")[1]
 
 print("This is prnumber!!!!!!!", pr_number)
-
-
-
-
-
+check_closed(pr_number)
 
