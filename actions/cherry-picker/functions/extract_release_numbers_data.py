80b2f5df37f9b1fec130147e88814b4d1db7b7e9
iancha1992 <heec@google.com>
2023-06-21 16:32:57 -0700

    Testing

--- actions/cherry-picker/functions/extract_release_numbers_data.py
+++ actions/cherry-picker/functions/extract_release_numbers_data.py
@@ -1,15 +1,17 @@
 import requests
+from pprint import pprint
 
 def get_all_milestones_data():
     headers = {
         'X-GitHub-Api-Version': '2022-11-28'
     }
     r = requests.get(f'https://api.github.com/repos/iancha1992/bazel/milestones', headers=headers)
-    print("tupac", r.json())
-    return r.json()["data"]
+    print("tupac")
+    pprint(r.json())
+    return r.json()
 
 
 def extract_release_numbers_data():
-    get_all_milestones_data()
+    milestones_data = get_all_milestones_data()
 
     
