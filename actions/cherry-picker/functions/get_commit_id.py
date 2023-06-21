02979a94effbd831d48eef85401d4df29c9ce88a
iancha1992 <heec@google.com>
2023-06-21 15:01:16 -0700

    Testing

--- actions/cherry-picker/functions/get_commit_id.py
+++ actions/cherry-picker/functions/get_commit_id.py
@@ -1,7 +1,7 @@
 import requests
 from pprint import pprint
 
-def get_commit_id(pr_number):
+def get_commit_id(pr_number, actor_name, action_event):
     headers = {
         'X-GitHub-Api-Version': '2022-11-28'
     }
@@ -11,3 +11,15 @@ def get_commit_id(pr_number):
     r = requests.get(f'https://api.github.com/repos/iancha1992/bazel/issues/{pr_number}/events', headers=headers, params=params)
     print("getcommitiddd")
     pprint(r.json())
+
+    commit_id = None
+
+    for event in r.json():
+        if (event["actor"]["login"] == actor_name) and (event["commit_id"] != None) and (commit_id == None) and (event["event"] == action_event):
+            commit_id = event["commit_id"]
+        elif (event["actor"]["login"] == actor_name) and (event["commit_id"] != None) and (commit_id != None) and (event["event"] == action_event):
+            raise ValueError(f'PR#{pr_number} has multiple commits by {actor_name}')
+    
+    if commit_id == None: raise ValueError(f'PR#{pr_number} has NO commit made by {actor_name}')
+
+    return commit_id
--- actions/cherry-picker/index.py
+++ actions/cherry-picker/index.py
@@ -6,7 +6,15 @@ token = os.environ["INPUT_TOKEN"]
 triggered_on = os.environ["INPUT_TRIGGERED_ON"]
 pr_number = os.environ["INPUT_PR_NUMBER"] if triggered_on == "closed" else os.environ["INPUT_PR_NUMBER"].split("#")[1]
 
+# actorName = "iancha1992";
+# actorName = "copybara-service[bot]";
+# actionEvent = "merged";
+# actionEvent = "closed";
+
+actor_name = "iancha1992"
+action_event = "merged"
+
 print("This is prnumber!!!!!!!", pr_number)
 if check_closed(pr_number) == False: raise ValueError(f'The PR #{pr_number} is not closed yet.')
 
-commit_id = get_commit_id(pr_number)
+commit_id = get_commit_id(pr_number, actor_name, action_event)
