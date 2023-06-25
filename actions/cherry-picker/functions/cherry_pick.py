5e803f680ca6befcb3feb1df98522820144e9f58
iancha1992 <heec@google.com>
2023-06-24 23:28:59 -0700

    Testing

--- actions/cherry-picker/functions/cherry_pick.py
+++ actions/cherry-picker/functions/cherry_pick.py
@@ -39,8 +39,8 @@ def cherry_pick(commit_id, pr_number, reviewers, release_number, issue_number):
     def checkout_release_number():
         print("git fetch --all")
         subprocess.run(['git', 'fetch', '--all'])  # Fetch all branches
-        print("git checkout", master_branch)
-        subprocess.run(['git', 'checkout', master_branch])
+        # print("git checkout", master_branch)
+        # subprocess.run(['git', 'checkout', master_branch])
         print(f'git checkout {release_branch_name}')
         subprocess.run(['git', 'checkout', release_branch_name])
         print(f'git checkout -b {target_branch_name}')
