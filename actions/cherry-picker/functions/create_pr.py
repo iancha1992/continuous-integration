3b744b824a529f644ba414b2b81de88899b1e1c1
iancha1992 <heec@google.com>
2023-06-26 22:12:52 -0700

    Testing

---
+++ actions/cherry-picker/functions/create_pr.py
@@ -0,0 +1,7 @@
+def create_pr(commit_id, pr_number, reviewers, release_number, issue_number):
+    print(commit_id, pr_number, reviewers, release_number, issue_number)
+
+
+
+
+create_pr("63a2d53", 103, [{'login': 'chaheein123', 'id': 23069091}], "6.3.0", 106)
