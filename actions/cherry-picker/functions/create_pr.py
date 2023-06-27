ad8d06cc81e033fed3476ade91afce7fee0f78b8
iancha1992 <heec@google.com>
2023-06-26 22:38:52 -0700

    testing

--- actions/cherry-picker/functions/create_pr.py
+++ actions/cherry-picker/functions/create_pr.py
@@ -1,7 +1,17 @@
-def create_pr(commit_id, pr_number, reviewers, release_number, issue_number):
+def create_pr(commit_id, pr_number, reviewers, release_number, issue_number, pr_url):
     print(commit_id, pr_number, reviewers, release_number, issue_number)
 
 
 
 
-create_pr("63a2d53", 103, [{'login': 'chaheein123', 'id': 23069091}], "6.3.0", 106)
+
+
+
+create_pr("63a2d53", 103, [{'login': 'chaheein123', 'id': 23069091}], "6.3.0", 106, "https://github.com/iancha1992/bazel/pull/new/cp103")
+
+
+
+
+
+
+
