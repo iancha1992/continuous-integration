95012516bca2c677470e7b7c483b9664cd757def
iancha1992 <heec@google.com>
2023-07-19 15:16:00 -0700

    Testing

--- actions/cherry-picker/functions/get_issue_data.py
+++ actions/cherry-picker/functions/get_issue_data.py
@@ -16,7 +16,7 @@ def get_issue_data(pr_number, commit_id):
         pr_body = original_msg[original_msg.index("\n\n") + 2:]
     else:
         pr_body = original_msg
-    commit_str_body = "Commit https://github.com/iancha1992/bazel/commit/some_commit_id"
+    commit_str_body = f"Commit https://github.com/iancha1992/bazel/commit/{commit_id}"
     if "PiperOrigin-RevId" in pr_body:
         piper_index = pr_body.index("PiperOrigin-RevId")
         pr_body = pr_body[:piper_index] + f"{commit_str_body}\n\n" + pr_body[piper_index:]
