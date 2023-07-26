dd3f823a2395de4f2f77990b8324b163a991bfe1
iancha1992 <heec@google.com>
2023-07-26 14:24:03 -0700

    testing

--- actions/cherry-picker/index.py
+++ actions/cherry-picker/index.py
@@ -40,6 +40,5 @@ for k in release_numbers_data.keys():
     release_number = k
     issue_number = release_numbers_data[k]
     pr_data = cherry_pick(commit_id, pr_number, reviewers, release_number, issue_number, is_first_time)
-    print(pr_data)
     create_pr(reviewers, release_number, issue_number, labels, issue_data, pr_data)
     is_first_time = False
