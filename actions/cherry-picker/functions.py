0ab76fd793ca75b56ea26d52404920e975f59f5e
iancha1992 <heec@google.com>
2023-07-28 15:34:33 -0700

    Testing

--- actions/cherry-picker/functions.py
+++ actions/cherry-picker/functions.py
@@ -106,10 +106,10 @@ def cherry_pick(commit_id, pr_number, release_number, issue_number, is_first_tim
         if status_checkout_release.returncode != 0:
             print(f"There is NO branch called {release_branch_name}...")
             print(f"Creating the {release_branch_name} from upstream, {upstream_url}")
-            subprocess.run(['git', 'remote', 'add', 'upstream', upstream_url], stdout = subprocess.DEVNULL, stderr = subprocess.DEVNULL)
-            subprocess.run(['git', 'fetch', 'upstream'], stdout = subprocess.DEVNULL, stderr = subprocess.DEVNULL)
-            subprocess.run(['git', 'branch', release_branch_name, f'upstream/{release_branch_name}'], stdout = subprocess.DEVNULL, stderr = subprocess.DEVNULL)
-            release_push_status = subprocess.run(['git', 'push', '--set-upstream', 'origin', release_branch_name], stdout = subprocess.DEVNULL, stderr = subprocess.DEVNULL)
+            subprocess.run(['git', 'remote', 'add', 'upstream', upstream_url], capture_output = True)
+            subprocess.run(['git', 'fetch', 'upstream'], capture_output = True)
+            subprocess.run(['git', 'branch', release_branch_name, f'upstream/{release_branch_name}'], capture_output = True)
+            release_push_status = subprocess.run(['git', 'push', '--set-upstream', 'origin', release_branch_name])
             if release_push_status.returncode != 0:
                 print("Did the push failed?")
                 result_data["is_successful"] = False
