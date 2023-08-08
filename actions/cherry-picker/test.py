59d94e6a32eb80d32895ceb2bfc8c4ce7331b1e7
iancha1992 <heec@google.com>
2023-08-08 13:11:40 -0700

    Testing

--- actions/cherry-picker/test.py
+++ actions/cherry-picker/test.py
@@ -22,9 +22,27 @@ import requests, subprocess
 
 # result = subprocess.run(['gh', 'api', '-H', 'Accept: application/vnd.github+json', '-H', 'X-GitHub-Api-Version: 2022-11-28', 'https://api.github.com/repos/bazelbuild/bazel/collaborators/brentleyjones'])  # Syncing
 
-print("~~~~~" * 100)
+# print("~~~~~" * 100)
 
-result = subprocess.run(['gh', 'api', '-XGET', '-H', 'Accept: application/vnd.github+json', '-H', 'X-GitHub-Api-Version: 2022-11-28', '-F', 'per_page=100', '-F', 'permission=admin', 'repos/bazelbuild/bazel/collaborators'])
+# result = subprocess.run(['gh', 'api', '-XGET', '-H', 'Accept: application/vnd.github+json', '-H', 'X-GitHub-Api-Version: 2022-11-28', '-F', 'per_page=100', '-F', 'permission=admin', 'repos/bazelbuild/bazel/collaborators'])
 # print(result.returncode)print
 
 # print("~~~~~" * 100)
+
+team_labels = ["team-CLI", "P3", "team-OSS", "team-Core", "P1", "team-Bazel", "team-Android", "area-java"]
+
+
+labels_list = list(filter(lambda label: "area" in label or "team" in label, team_labels))
+
+
+
+
+
+print(labels_list)
+
+
+
+
+
+
+
