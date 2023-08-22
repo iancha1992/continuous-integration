95236d876ad317975b20d75e9ba40b9cd4851730
iancha1992 <heec@google.com>
2023-08-22 14:17:56 -0700

    Testing

--- actions/cherry-picker/index.py
+++ actions/cherry-picker/index.py
@@ -72,6 +72,6 @@ for k in release_numbers_data.keys():
         create_pr(reviewers, release_number, issue_number, labels, pr_title_body, release_branch_name, target_branch_name, input_data["user_name"], input_data["api_repo_name"], input_data["is_prod"])
     except Exception as e:
         print("Cherrypick error!")
-        print(e)
-        issue_comment(issue_number, e, input["api_repo_name"], input["is_prod"])
+        print(str(e))
+        issue_comment(issue_number, str(e), input["api_repo_name"], input["is_prod"])
     is_first_time = False
