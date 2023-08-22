926549e2e02d6a3f0ddab305a58ac5221683ede3
iancha1992 <heec@google.com>
2023-08-22 13:53:19 -0700

    Testing

--- actions/cherry-picker/index.py
+++ actions/cherry-picker/index.py
@@ -68,9 +68,7 @@ for k in release_numbers_data.keys():
     issue_number = release_numbers_data[k]
     try:
         cherrypick_status_code = cherry_pick(commit_id, release_branch_name, target_branch_name, issue_number, is_first_time, input_data)
+        create_pr(reviewers, release_number, issue_number, labels, pr_title_body, release_branch_name, target_branch_name, input_data["user_name"], input_data["api_repo_name"], input_data["is_prod"])
     except Exception as e:
         print(e)
-        cherrypick_status_code = 1
-    if cherrypick_status_code == 0:
-        create_pr(reviewers, release_number, issue_number, labels, pr_title_body, release_branch_name, target_branch_name, input_data["user_name"], input_data["api_repo_name"], input_data["is_prod"])
     is_first_time = False
