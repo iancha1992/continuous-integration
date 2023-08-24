8ff58ead3be0edf860b108dbd1d4dad0e2df6390
iancha1992 <heec@google.com>
2023-08-24 13:45:32 -0700

    testing

--- actions/cherry_picker/cherrypicker_ondemand.py
+++ actions/cherry_picker/cherrypicker_ondemand.py
@@ -4,7 +4,9 @@ issue_body = os.environ["INPUT_ISSUE_BODY"]
 # issue_body = "@bazel-io cherry-pick\r\ncommits: xyz, abc\r\nreviewers: @iancha1992\r\nteam: team-ExternalDeps"
 
 print("ONDEMAND LUNCH!")
-print("issue_body", issue_body)
+print("issue_body")
+print("banana")
+print(issue_body)
 
 
 
