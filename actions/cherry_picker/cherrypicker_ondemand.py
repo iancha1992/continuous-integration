731104d6127de651200bbf8a2f013eb5f77e085d
iancha1992 <heec@google.com>
2023-08-24 16:17:50 -0700

    Testing

--- actions/cherry_picker/cherrypicker_ondemand.py
+++ actions/cherry_picker/cherrypicker_ondemand.py
@@ -1,10 +1,11 @@
 import os
 from vars import input_data
-# from functions import cherry_pick
+from functions import cherry_pick
 
 milestone_title = os.environ["INPUT_MILESTONE_TITLE"]
 milestoned_issue_number = os.environ["INPUT_MILESTONED_ISSUE_NUMBER"]
-issue_body = "@bazel-io cherry-pick\r\ncommits: xyz, abc\r\nreviewers: @iancha1992\r\nteam: team-ExternalDeps"
+issue_body = os.environ["INPUT_ISSUE_BODY"]
+# issue_body = "@bazel-io cherry-pick\r\ncommits: xyz, abc\r\nreviewers: @iancha1992\r\nteam: team-ExternalDeps"
 
 issue_body_split = issue_body.split("\r\n")
 
@@ -18,16 +19,11 @@ for info in issue_body_split:
     elif "team" in info.lower().split(":")[0]:
         issue_body_dict["labels"] = info[info.index(":") + 2:].replace(" ", "").split(",")
 
-print(issue_body_split)
+print("issue_body_dict")
 print(issue_body_dict)
 
-
-
-
-
 for commit_id in issue_body_dict["commits"]:
-
-
-    print(commit_id)
-    # print(input_data)
-
+    release_number = milestone_title.split(" release blockers")[0]
+    release_branch_name = f"{input_data['release_branch_name_initials']}{release_number}"
+    target_branch_name = f"cp{milestoned_issue_number}-{release_number}"
+    cherry_pick(commit_id, release_branch_name, target_branch_name, True, input_data)
