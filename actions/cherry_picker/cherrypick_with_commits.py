aad2da0f2f1b85597ba0cc859428105bdfd124c4
iancha1992 <heec@google.com>
2023-09-01 18:16:39 -0700

    Testing

--- actions/cherry_picker/cherrypick_with_commits.py
+++ actions/cherry_picker/cherrypick_with_commits.py
@@ -62,12 +62,12 @@ if len(successful_commits):
             issue_comment_body += ", "
 
     if len(failed_commits):
-        failure_commits_str = f"It also has {len(failed_commits)} failed commits, "
+        failure_commits_str = f"It also has {len(failed_commits)} failed commits: "
         for fail_commit in failed_commits:
             failure_commits_str += f"https://github.com/{input_data['api_repo_name']}/commit/{fail_commit['commit_id']} ({fail_commit['msg']})"
             if idx < len(failed_commits) - 1:
                 failure_commits_str += ", "
-        failure_commits_str += ", which are not included in the PR."
+        failure_commits_str += "These failed commits are NOT included in the PR."
         issue_comment_body += failure_commits_str
 
 else:
