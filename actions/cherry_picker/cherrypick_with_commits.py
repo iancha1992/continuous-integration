cd27fb845b49d41d46c5871f5110daa679645c50
iancha1992 <heec@google.com>
2023-09-01 18:01:50 -0700

    Testing

--- actions/cherry_picker/cherrypick_with_commits.py
+++ actions/cherry_picker/cherrypick_with_commits.py
@@ -56,6 +56,7 @@ if len(successful_commits):
 
     success_commits_str = " ("
     for index, success_commit in enumerate(successful_commits):
+        print("This is index!!", index)
         success_commits_str += f"https://github.com/{input_data['api_repo_name']}/commit/{success_commit['commit_id']}"
         if index < len(success_commit) - 1:
             success_commits_str += ", "
